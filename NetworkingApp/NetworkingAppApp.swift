529750c8a6150fc658c8f76e83b288f74b9e3137
Daria Yats <dariayatsyniuk@gmail.com>
2025-07-14 13:18:22 +0200

    Initial Commit

---
+++ NetworkingApp/NetworkingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkingAppApp.swift
+//  NetworkingApp
+//
+//  Created by Daria Yatsyniuk on 14.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct NetworkingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
