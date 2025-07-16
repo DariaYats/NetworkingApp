00b01ee10f12ebcb52743d65c67663ed72715a66
Daria Yats <dariayatsyniuk@gmail.com>
2025-07-16 11:45:43 +0200

    initial commit

--- NetworkingApp/ContentView.swift
+++ NetworkingApp/ContentView.swift
@@ -5,17 +5,48 @@
 //  Created by Daria Yatsyniuk on 14.07.2025.
 //
 
+import SwiftData
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showingPhotoPicker = false
+    @State private var showingAddContactView = false
+    @State private var selectedImage: UIImage?
+
+    @Environment(\.modelContext) private var modelContext
+    @Query private var contacts: [Contact]
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            List {
+                ForEach(contacts) { contact in
+                    NavigationLink(destination: DetailView()) {
+                        HStack(spacing: 40) {
+                            Text(contact.name)
+                                .font(.subheadline)
+                        }
+                    }
+                }
+            }
+            .navigationTitle("NetworkingApp")
         }
-        .padding()
+
+        Button("Add new photo") {
+            showingPhotoPicker = true
+        }
+        .sheet(isPresented: $showingPhotoPicker) {
+            PhotoPickerView { image in
+                selectedImage = image
+                if image != nil {
+                    showingAddContactView = true
+                }
+            }
+        }
+
+//        .sheet(item: $selectedImage) { image in
+//            AddContactView()
+//        }
+
     }
 }
 
