646a712813d8e79ec0007213a2d51f5101c9c580
Daria Yats <dariayatsyniuk@gmail.com>
2025-07-29 16:45:29 +0200

    add function to save image

--- NetworkingApp/AddContactView.swift
+++ NetworkingApp/AddContactView.swift
@@ -11,6 +11,9 @@ struct AddContactView: View {
     let image: UIImage
     @State private var name = ""
 
+    @Environment(\.modelContext) private var modelContext
+    @Environment(\.dismiss) private var dismiss
+
     var body: some View {
         VStack(spacing: 40) {
             Image(uiImage: image)
@@ -22,13 +25,39 @@ struct AddContactView: View {
                 .font(.headline)
 
             Button("Save") {
-
+                saveContact()
+                dismiss()
             }
         }
     }
 
     func saveContact() {
-        
+        guard !name.isEmpty else {
+            // show error alert
+            return
+        }
+        guard let imageName = saveImage() else {
+            return
+        }
+        let contact = Contact(id: UUID(), name: name, imageName: imageName)
+        modelContext.insert(contact)
+    }
+
+    func saveImage() -> String? {
+        guard let imageData = image.jpegData(compressionQuality: 1) else {
+            return nil
+        }
+
+        let documentsURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
+        let uniqueFileName = UUID().uuidString + ".jpg"
+        let fileURL = documentsURL.appendingPathComponent(uniqueFileName)
+        do {
+            try imageData.write(to: fileURL)
+            return uniqueFileName
+        } catch {
+            // show saving error
+            return nil
+        }
     }
 }
 
